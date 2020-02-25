25baaf8bebd72e0d6ece1879219b2b821f4dd31e
nmarno68 <narnold@nevada.unr.edu>
2020-02-25 15:31:55 -0800

    init

---
+++ ourBoi/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/narnold/Workspaces/class-activity-5-arnold-black/ourBoi/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
